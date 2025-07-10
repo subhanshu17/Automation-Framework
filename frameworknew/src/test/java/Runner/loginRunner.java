22c9543b1aee29bb2ae34eeda760935561e8bedd
17sub <17sub@home>
2025-07-10 11:38:09 +0530

    aaaa

--- frameworknew/src/test/java/Runner/loginRunner.java
+++ frameworknew/src/test/java/Runner/loginRunner.java
@@ -1,12 +1,12 @@
 package Runner;
 
 
-import org.junit.runner.RunWith;
 
-import io.cucumber.junit.*;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
 
-@RunWith(Cucumber.class)
+
 @CucumberOptions(
     features = "src/test/Resource/login",
   glue = {"stepDefinition"},
@@ -15,6 +15,6 @@ import io.cucumber.junit.*;
     tags= "@tag1"
 )
 
-public class loginRunner {
+public class loginRunner extends AbstractTestNGCucumberTests{
 
 }
